6787e35e98c868928d0d700d85d9a6ec34ab6fcc
Soham <sohammafidar2000@gmail.com>
2023-11-27 21:25:35 +0530

    partial fix no audio message in UI

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,33 +4,43 @@ import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 function Hero(props) {
     const [isPlaying, setIsPlaying] = React.useState(false);
-    const audioWord = new Audio(props.audio);
+    //TODO Add a conditional check to see if audio is not empty.
+    const audioWord = props.audio === "" ? "" : new Audio(props.audio);
 
     const handleClick = () => {
-        setIsPlaying((prevState) => {
-            const playState = !prevState;
-            if (playState) {
-                audioWord.play();
-            }
-            return playState;
-        });
-
+        if (audioWord !== "") {
+            setIsPlaying((prevState) => {
+                const playState = !prevState;
+                if (playState) {
+                    audioWord.play();
+                }
+                return playState;
+            });
+        }
     };
-
     return (
         <div className="hero">
             <h1>
                 {props.word}
             </h1>
             <div className="play-btn">
-                {isPlaying ? (
-                    <button id="play" onClick={handleClick}><FontAwesomeIcon id='play-svg' icon={faPause} style={{ color: "#b734ac", }} /></button>
-                ) : (
-                    <button id="play" onClick={handleClick}><FontAwesomeIcon id='play-svg' icon={faPlay} style={{ color: "#b734ac", }} /></button>
-                )}
+                {
+                    audioWord !== "" && isPlaying ? (<button id="play" onClick={handleClick}><FontAwesomeIcon id='play-svg' icon={faPause} style={{ color: "#b734ac", }} /></button>)
+                        : (audioWord !== "" && !isPlaying) ? (
+                            <button id="play" onClick={handleClick}><FontAwesomeIcon id='play-svg' icon={faPlay} style={{ color: "#b734ac", }} /></button>
+                        ) : (
+                            <button id="play" onClick={handleClick}><FontAwesomeIcon id='play-svg' icon={faPlay} style={{ color: "#b734ac", }} /></button>
+                        )
+                }
+
+
+
             </div>
         </div>
     )
 }
 
+
+
 export default Hero
+
